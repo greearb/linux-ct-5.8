5300f7538fc03f373ac89acf9c0c61fcd3243e5f
Ben Greear <greearb@candelatech.com>
2015-11-04 06:11:13 -0800

    netlink: Add pr_err calls for netlink parsing errors.
    This could be noisy if there are buggy user-space applications,
    but it is a big help when you are trying to fix one of those
    buggy applications.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- lib/nlattr.c
+++ lib/nlattr.c
@@ -352,8 +352,10 @@ static int validate_nla(const struct nlattr *nla, int maxtype,
 		goto out_err;
 
 	case NLA_FLAG:
-		if (attrlen > 0)
+		if (attrlen > 0) {
+			pr_err("validate-nla:  FLAG len is too large: %d\n", attrlen);
 			goto out_err;
+		}
 		break;
 
 	case NLA_BITFIELD32:
@@ -373,13 +375,16 @@ static int validate_nla(const struct nlattr *nla, int maxtype,
 
 		if (!minlen || memchr(nla_data(nla), '\0', minlen) == NULL) {
 			err = -EINVAL;
+			pr_err("validate-nla:  NUL-STRING parse error, minlen: %d\n", minlen);
 			goto out_err;
 		}
 		/* fall through */
 
 	case NLA_STRING:
-		if (attrlen < 1)
+		if (attrlen < 1) {
+			pr_err("validate-nla:  STRING too short, attrlen: %d\n", attrlen);
 			goto out_err;
+		}
 
 		if (pt->len) {
 			char *buf = nla_data(nla);
@@ -387,14 +392,20 @@ static int validate_nla(const struct nlattr *nla, int maxtype,
 			if (buf[attrlen - 1] == '\0')
 				attrlen--;
 
-			if (attrlen > pt->len)
+			if (attrlen > pt->len) {
+				pr_err("validate-nla:  STRING length err, attrlen: %d pt->len: %d\n",
+				       attrlen, pt->len);
 				goto out_err;
+			}
 		}
 		break;
 
 	case NLA_BINARY:
-		if (pt->len && attrlen > pt->len)
+		if (pt->len && attrlen > pt->len) {
+			pr_err("validate-nla:  BINARY length err, attrlen: %d pt->len: %d\n",
+			       attrlen, pt->len);
 			goto out_err;
+		}
 		break;
 
 	case NLA_NESTED:
@@ -403,8 +414,11 @@ static int validate_nla(const struct nlattr *nla, int maxtype,
 		 */
 		if (attrlen == 0)
 			break;
-		if (attrlen < NLA_HDRLEN)
+		if (attrlen < NLA_HDRLEN) {
+			pr_err("validate-nla:  Nested attrlen < hdrlen: %d < %d\n",
+			       attrlen, NLA_HDRLEN);
 			goto out_err;
+		}
 		if (pt->nested_policy) {
 			err = __nla_validate_parse(nla_data(nla), nla_len(nla),
 						   pt->len, pt->nested_policy,
@@ -468,8 +482,11 @@ static int validate_nla(const struct nlattr *nla, int maxtype,
 		else
 			minlen = nla_attr_minlen[pt->type];
 
-		if (attrlen < minlen)
+		if (attrlen < minlen) {
+			pr_err("validate-nla:  DEFAULT (%d) lenght error, attrlen: %d  minlen: %d\n",
+			       pt->type, attrlen, minlen);
 			goto out_err;
+		}
 	}
 
 	/* further validation */
